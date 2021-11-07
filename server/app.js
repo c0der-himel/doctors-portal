7504fb7b0d404a34bf1805ac1f5326519654ee78
himel <himel777hussain@gmail.com>
2021-11-07 15:41:05 +0600

    uploading

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+const uri = `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.qctkg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    console.log('Database Connected');
+  } finally {
+    // await client.close();
+  }
+};
+
+run().catch(console.dir);
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
